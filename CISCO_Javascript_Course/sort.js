48babb2fc44e53c200e1340d28fc1b2541bca7db
planner-sohrab <plannersohrab@gmail.com>
2023-09-21 13:42:21 +0600

    sort.js added with initial code

---
+++ CISCO_Javascript_Course/sort.js
@@ -0,0 +1,36 @@
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+}, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu"
+}];
+let showContact = function(contacts, i) {
+    if (contacts instanceof Array && contacts[i]) {
+        console.log(`${contacts[i].name} / ${contacts[i].phone} / ${contacts[i].email}`);
+    }
+}
+
+let showAllContacts = function(contacts) {
+    if (contacts instanceof Array) {
+        for (contact of contacts) {
+            console.log(`${contact.name} / ${contact.phone} / ${contact.email}`);
+        }
+    }
+}
+
+let addNewContact = function(contacts, name, phone, email) {
+    if (contacts instanceof Array && name && phone && email) {
+        contacts.push({
+            name: name,
+            phone: phone,
+            email: email
+        });
+    }
+}
