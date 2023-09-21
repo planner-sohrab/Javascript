3a8e88d9cf4f02e6bece6c3f99a443e5af2ff4b8
planner-sohrab <plannersohrab@gmail.com>
2023-09-21 13:37:22 +0600

    blog post slightly modified

--- CodeWithMosh/5 Objects/Blog Post Object.js
+++ CodeWithMosh/5 Objects/Blog Post Object.js
@@ -3,12 +3,10 @@ const post = {
     body: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.",
     author: "Myself",
     views: 3405,
-    comments: {
-        author: "M",
-        body: "Nice Post"
-    },
+    comments: [{author: "M", body: "Nice Post"},
+        {author: "c", body: "d Post"},],
     isLive : true
 };
 
 console.log(post)
-console.log(post.comments)
+console.log(post.comments[0])
