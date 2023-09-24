c815a7807d5ce4060a699f81fd07fa4132cf239e
planner-sohrab <plannersohrab@gmail.com>
2023-09-24 13:46:10 +0600

    ArrayMethods.js added

--- CodeWithMosh/06 Array/ArrayMethods.js
+++ CodeWithMosh/06 Array/ArrayMethods.js
@@ -2,4 +2,13 @@ const numbers   = [3,5];
 //Adding elements in an array
 //end
 numbers.push(6,7);
+console.log(numbers);
+
+//middle
+
+numbers.splice(2,0,4,4.5);
+console.log(numbers)
+
+// start
+numbers.unshift("a","b","c","d", "e");
 console.log(numbers)
