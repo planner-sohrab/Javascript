b17bc96d190e13dbf75e5b7347a37a96e4d8ee4d
planner-sohrab <plannersohrab@gmail.com>
2023-09-26 09:59:00 +0600

    Ex_Number of occurance.js added

--- CodeWithMosh/06 Array/Ex_Number of occurance.js
+++ CodeWithMosh/06 Array/Ex_Number of occurance.js
@@ -8,7 +8,16 @@ function occurance(array, searchElement){
     }
     return count;
 }
-
 const numbers = [1,2,3,4, 1,1,-1];
-const countOfOccurance = occurance(numbers, -2);
+const countOfOccurance = occurance(numbers, 1);
 console.log(countOfOccurance);
+//solution 2 using Reduce method
+function occuranceNumber(array, searchElement){
+    return array.reduce((accumulator, current)=>{
+        let occurance = (current === searchElement)? 1: 0;
+        return accumulator+occurance;
+    },0);
+}
+
+const countOfOccurance2 = occuranceNumber(numbers, 1);
+console.log(countOfOccurance2);
