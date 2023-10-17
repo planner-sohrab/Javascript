0aa7ef382ad3bb9b768b92bb3206124dd67400b3
planner-sohrab <plannersohrab@gmail.com>
2023-10-17 10:55:03 +0600

    Social media site added

--- Youtube/Social Media/script.js
+++ Youtube/Social Media/script.js
@@ -1,4 +1,10 @@
 let settingsmenu = document.querySelector(".settings-menu")
+let darkBtn = document.getElementById("dark-btn")
 function settingsMenuToggle(){
     settingsmenu.classList.toggle("settings-menu-height")
 }
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+}
+
