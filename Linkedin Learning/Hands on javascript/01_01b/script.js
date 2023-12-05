dd19cd0a3cf1f5dc2dc1cee489086b264e8a3775
planner-sohrab <plannersohrab@gmail.com>
2023-12-05 11:44:24 +0600

    Javascript essential started

---
+++ Linkedin Learning/Hands on javascript/01_01b/script.js
@@ -0,0 +1,7 @@
+/**
+ * Add date to output.
+ * References:
+ * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+ * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+ */
+
