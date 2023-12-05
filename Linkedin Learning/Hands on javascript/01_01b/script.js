e9578dabbf0f3d4c8d8237fe3536abebafcb1e88
planner-sohrab <plannersohrab@gmail.com>
2023-12-05 11:46:23 +0600

    Javascript essential started

--- Linkedin Learning/Hands on javascript/01_01b/script.js
+++ Linkedin Learning/Hands on javascript/01_01b/script.js
@@ -5,3 +5,10 @@
  * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
  */
 
+import data from "./data.js";
+
+const mainContent =document.querySelector(".main-content");
+
+const Card = (data) => {
+    const imgData = data[0];
+}
