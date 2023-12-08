bcef5b49afa62eddf17bddd6d128f74faa21f338
Sohrab <plannersohrab@gmail.com>
2023-12-09 00:09:26 +0600

    javascript

--- JS Mastery/Test/test.js
+++ JS Mastery/Test/test.js
@@ -1,13 +1,28 @@
-// console.log("Hello world!");
-
+//Showing array information in HTML Unordered List
 const fruits = ["Apple", "Papaya", "Orange", "Karambola"];
-const fruitN01 = fruits.forEach((fruit) => {
-    return ("Hello this is:" + fruit);
+let list = document.getElementById("myList");
+//Method 1: Using for Loop
+
+for (let i = 0; i < fruits.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = fruits[i];
+    list.appendChild(li);
+}
+
+
+// Method 2: Using forEach method
+fruits.forEach((fruit) => {
+    let li = document.createElement('li');
+    li.innerText = fruit;
+    list.appendChild(li)
 });
 
-const fruitN02 = fruits.map((fruit) => {
-    return ("This is :" + fruit);
-});
 
-// console.log(fruitN01);
-console.log(fruitN02);
+//Method 3: Using join method
+
+let ul = `
+<ul>${fruits.map(data =>
+    `<li>${data}</li>`).join('')}
+</ul>`;
+
+list.innerHTML = ul;
