78cb95705f42110645702af3c44cd9db97f1bb4d
Sohrab <plannersohrab@gmail.com>
2023-12-08 23:00:27 +0600

    javascript

--- JS Mastery/Test/test.js
+++ JS Mastery/Test/test.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+// console.log("Hello world!");
+
+const fruits = ["Apple", "Papaya", "Orange", "Karambola"];
+const fruitN01 = fruits.forEach((fruit) => {
+    return ("Hello this is:" + fruit);
+});
+
+const fruitN02 = fruits.map((fruit) => {
+    return ("This is :" + fruit);
+});
+
+console.log(fruitN01);
+console.log(fruitN02);
