5365947700d62f510364d371b7f1a85d248eaf39
planner-sohrab <plannersohrab@gmail.com>
2023-12-12 12:41:56 +0600

    Input and ListView added

--- PacktPub/JavaScript DOM Projects/Project 1/list.js
+++ PacktPub/JavaScript DOM Projects/Project 1/list.js
@@ -1,1 +1,31 @@
-const output = document.querySelector('.output')
+const output = document.querySelector('.output');
+const myInput = createMyElement(output, 'input', 'main');
+myInput.setAttribute('type', 'text');
+
+const myBtn = createMyElement(output, 'button', 'btn');
+myBtn.textContent = 'Add New User';
+
+const myList = createMyElement(output, 'ul', 'myList');
+
+myBtn.addEventListener('click', (e) => {
+    console.log('click');
+    let userName = myInput.value;
+    if(userName.length > 3){
+        const li = addNewUser(userName);
+        myInput.value = '';
+    }
+
+})
+
+function addNewUser(userName){
+    const li = createMyElement(myList, 'li', 'myList');
+    li.textContent = userName;
+    return li;
+}
+
+function createMyElement(parent, elType, classAdd){
+    const ele = document.createElement(elType);
+    parent.append(ele);
+    ele.classList.add(classAdd);
+    return ele;
+}
