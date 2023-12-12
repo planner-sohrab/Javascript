cda9cf373dbc91e6e5c5079f4cb9273b28c0a720
planner-sohrab <plannersohrab@gmail.com>
2023-12-12 09:56:45 +0600

    Javascript style

---
+++ PacktPub/JavaScript DOM Projects/Exercise 2/ex2.js
@@ -0,0 +1,17 @@
+const output = document.querySelector('#myId');
+const html = "<h1> Hello World</h1>";
+output.textContent = html;
+output.innerHTML = html;
+
+output.style.color = 'white';
+output.style.backgroundColor='coral';
+output.style.border = '2px solid black';
+output.style.borderRadius = '25px';
+output.style.paddingLeft = "1rem";
+
+
+
+
+
+
+
