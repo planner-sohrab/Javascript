b94fa15bf2581e9626dca5ba400d245998809226
planner-sohrab <plannersohrab@gmail.com>
2023-12-14 15:33:46 +0600

    Edit and Delete Button Added

--- PacktPub/JavaScript DOM Projects/Star/star.js
+++ PacktPub/JavaScript DOM Projects/Star/star.js
@@ -2,7 +2,10 @@ const main = document.querySelector(".stars");
 const starsUl= createElements(main, 'ul', 'main')
 const output = createElements(main, 'div', 'output')
 
+for(let x=0; x<5; x++){
+    const star = createElements(starsUl, 'li', 'star');
 
+}
 function createElements(parent, elType, myClass){
     const el = document.createElement(elType);
     parent.append(el);
