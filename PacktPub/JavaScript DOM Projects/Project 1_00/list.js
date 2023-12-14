fab313a45184bd6bfef1240feb9b5aefd7a09f46
planner-sohrab <plannersohrab@gmail.com>
2023-12-14 15:04:51 +0600

    Edit and Delete Button Added

--- PacktPub/JavaScript DOM Projects/Project 1_00/list.js
+++ PacktPub/JavaScript DOM Projects/Project 1_00/list.js
@@ -4,16 +4,40 @@ myInput.setAttribute('type', 'text');
 const myBtn = createMyElement(output, 'button', 'btn');
 myBtn.textContent = 'Add New User';
 
+
+const warning = createMyElement(output, 'p', 'warning');
+
+// warning.style.backgroundColor='coral';
+// warning.style.padding='1rem';
+// warning.style.borderRadius='25px';
+// warning.style.color='white';
+// warning.style.textAlign='center';
+
 const ul = createMyElement(output, 'ul', 'myList');
 
 myBtn.addEventListener('click', () => {
-    const li = addUserName(myInput.value)
-    myInput.value = "";
+    let userName = myInput.value;
+    if(userName.length >= 3){
+        const li = addUserName(userName)
+        myInput.value = "";
+    }else{
+        warning.textContent = "User name required at least 3 characters!"
+    }
+
+
 })
 
 function addUserName(userName) {
     const li = createMyElement(ul, 'li', 'list-item');
-    li.textContent = userName;
+    const div = createMyElement(li, 'div', 'container');
+    const spanInfo = createMyElement(div, 'span', 'info');
+    spanInfo.textContent = userName;
+    const innerDiv = createMyElement(div, 'div', 'inner-div');
+    const spanEdit = createMyElement(innerDiv, 'span', 'editor');
+    spanEdit.textContent = 'Edit';
+    const spanDelete = createMyElement(innerDiv, 'span', 'deletor');
+    spanDelete.textContent = 'Delete';
+
     return li;
 }
 
